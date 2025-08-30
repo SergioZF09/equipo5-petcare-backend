b6b3581300422abe7ffa8763311e342bd2321924
Christian Cachero <Ch_canchero@live.com.ar>
2025-08-30 00:51:32 -0300

    Se arregló el bug que estaba ocasionando los errores en esta rama del compa Sergio.
    El problema se debía a versiones incompatibles de los plugins de lombok se corrigió eso.
    
    Para el compa sergio tuve que comentar algunos de sus métodos del repository, ya que daban error porque no hay una entidad llamada Sitters en Booking.
    
    Para el QA se necesitan actualizar los test, ya que con los cambios daban errores y no dejaban que ejecutara correctamente el proyecto.
    
    Con estos ajustes aplicados debería de funcionar bien tanto si lo ejecutas con el botón de play como desde maven o desde la terminal.
    
    Atte: Christian Cachero

--- src/main/java/com/equipo5/backend/model/dtos/request/PetRequestDTO.java
+++ src/main/java/com/equipo5/backend/model/dtos/request/PetRequestDTO.java
@@ -4,13 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record PetRequestDTO(
+        @NotNull(message = "Incomplete attribute: ownerId")
+        Long ownerId,
         @NotBlank(message = "Incomplete attribute: 'name'")
         String name,
         @NotBlank(message = "Incomplete attribute: 'species'")
         String species,
         @NotBlank(message = "Incomplete attribute: 'breed'")
-        String breed,
-        @NotNull(message = "Incomplete attribute: ownerId")
-        Long ownerId
+        String breed
         ) {
 }
