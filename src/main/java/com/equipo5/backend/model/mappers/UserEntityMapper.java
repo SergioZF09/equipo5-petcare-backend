e862f9474c67e7ed4a4ea3e31347212c1893562a
matiasnm <matiasnm@gmail.com>
2025-09-04 17:37:58 -0300

    fix: Se agregan 3 DTOS modulares(no tienen relaciones extras) para el UserResponseDTO y se actualizan metodos en UserEntityMapper. Se actualizan los Test Unitarios de UserEntity y UserServiceImpl.

--- src/main/java/com/equipo5/backend/model/mappers/UserEntityMapper.java
+++ src/main/java/com/equipo5/backend/model/mappers/UserEntityMapper.java
@@ -1,8 +1,14 @@
 package com.equipo5.backend.model.mappers;
 
+import com.equipo5.backend.model.Booking;
+import com.equipo5.backend.model.Pet;
+import com.equipo5.backend.model.ServiceEntity;
 import com.equipo5.backend.model.UserEntity;
 import com.equipo5.backend.model.dtos.request.UserRequestDTO;
+import com.equipo5.backend.model.dtos.response.UserResponseBookingsDTO;
 import com.equipo5.backend.model.dtos.response.UserResponseDTO;
+import com.equipo5.backend.model.dtos.response.UserResponsePetsDTO;
+import com.equipo5.backend.model.dtos.response.UserResponseServicesDTO;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -15,4 +21,10 @@ public interface UserEntityMapper {
     UserResponseDTO toUserDTO(UserEntity user);
 
     List<UserResponseDTO> toUserDTOs(List<UserEntity> user);
+
+    List<UserResponsePetsDTO> toUserPetsDTOs(List<Pet> pet);
+
+    List<UserResponseBookingsDTO> toUserBookingsDTOs(List<Booking> booking);
+
+    List<UserResponseServicesDTO> toUserResponseServicesDTOs(List<ServiceEntity> userResponseServices);
 }
